f1eb350b2dc15b4e683b340ce147c82a8230ff7d
joshuavottero <joshuavottero@outlook.com>
2021-11-23 23:59:27 +0000

    added assertArraysEqual checks if two arrays are the same

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+const eqArray = function(items1, items2) {
+  // if arrays are differnt lengths they cant be the same
+  if (items1.length !== items2.length) {
+    return false;
+  }
+  for (let i = 0; i < items1.length; i++) {
+    if (items1[i] !== items2[i]) {
+      // if any element is not the same the arrays are differnt
+      return false;
+    }
+  }
+  // if code gets here then they are the same
+  return true;
+};
+
+const assertArraysEqual = function(items1, items2) {
+  if (eqArray(items1, items2) === true) {
+    console.log(`👍Both Arrays are the same: ${items1} === ${items2}`);
+  } else {
+    console.log(`👎The Arrays are not the same: ${items1} !== ${items2}`);
+  }
+};
+assertArraysEqual([1, 2], [1, 2]);
+assertArraysEqual([1, 2], [1, 2, 3]);
+assertArraysEqual([1], [1, 2]);
